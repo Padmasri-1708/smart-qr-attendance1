11407e1edad66b87ace84fe498e7858d6f7a1b93
Padmasri <jjaya6257@gmail.com>
2026-08-30 11:39:29 +0530

    Fix India timezone

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, request, redirect, session
 import mysql.connector
-from datetime import datetime, time
+from datetime import datetime, time,timedelta
 from math import radians, sin, cos, sqrt, atan2
 
 
@@ -421,7 +421,7 @@ def mark_attendance():
 
     # CURRENT DATE AND TIME
 
-    now = datetime.now()
+    now = datetime.utcnow()+timedelta(hours=5,minutes=30)
 
     current_date = now.date()
     current_time = now.time()
