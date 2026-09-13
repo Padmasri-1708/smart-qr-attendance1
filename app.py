284dad580b31fcb79e4b55df9c5f09113459bf44
Padmasri <jjaya6257@gmail.com>
2026-09-13 08:53:12 +0530

    Update test location

--- app.py
+++ app.py
@@ -680,9 +680,9 @@ def mark_attendance():
     # COLLEGE LOCATION
     # =================================================
 
-    COLLEGE_LAT = 12.671705
+    COLLEGE_LAT = 12.554142
 
-    COLLEGE_LON = 77.965916
+    COLLEGE_LON = 78.015793
 
     ALLOWED_RADIUS = 200
 
